a6dd4e3daa517095973761121ab65b94cad0fad2
Miguel Escobar <mescoba1@uci.edu>
2025-12-11 21:21:32 -0800

    update docker entry to create cron log file.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# Start cron in the foreground
-crond -f -L /var/log/cron.log &
+# 1. Create the log file so 'tail' doesn't fail
+touch /var/log/cron.log
 
-# Keep the container running indefinitely
+# 2. Start cron in the background and pipe its output to the file
+#    The '-L' option is removed as we are manually piping the output to the file
+crond -f 2>&1 | tee -a /var/log/cron.log &
+
+# 3. Wait a moment for crond to start up before checking logs
+sleep 5
+
+# 4. Keep the container running indefinitely by tailing the log
+#    This is now safe because the file exists.
+echo "Cron daemon started. Tailing /var/log/cron.log..."
 tail -f /var/log/cron.log
